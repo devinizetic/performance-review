2e5aec0cd2e0541bc96b2442c1dcff0f4eebbffe
Ivan Nizetic <ivan.nizetic@gmail.com>
2024-01-10 12:49:27 -0300

    Fix: Added correct seed method

--- supabase/migrations/seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Roles (name) VALUES ('admin')
-INSERT INTO Roles (name) VALUES ('reviewer')
-INSERT INTO Roles (name) VALUES ('reviewee')
---
+++ supabase/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO Roles (name) VALUES ('admin')
+INSERT INTO Roles (name) VALUES ('reviewer')
+INSERT INTO Roles (name) VALUES ('reviewee')
