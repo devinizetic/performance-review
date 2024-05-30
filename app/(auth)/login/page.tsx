f95383299708a589a6214855e0fabf5977ecf0b0
Ivan Nizetic <ivan.nizetic@gmail.com>
2024-05-30 17:12:49 -0300

    Update: Login button styling and progressbar styling

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import { createServerClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import Image from 'next/image';
 import googleLogo from '../../g-logo.png';
+import LoginButton from './components/LoginButton';
 
 export default async function Login({
   searchParams
@@ -18,7 +19,6 @@ export default async function Login({
   const signInWithGoogle = async () => {
     'use server';
 
-    const cookieStore = cookies();
     const supabase = createServerClient();
 
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -36,15 +36,9 @@ export default async function Login({
   };
 
   return (
-    <form
-      className="flex h-screen items-end justify-center"
-      action={signInWithGoogle}
-    >
+    <form className="flex h-screen items-end justify-center">
       <div className="flex items-center justify-center h-1/2">
-        <button className="bg-[#fff] border-[#d3d3d3] border-solid border-[1px] rounded-[4px] text-[#737373] cursor-pointerm-0 px-3 py-3 text-center flex items-center justify-center gap-3">
-          <Image src={googleLogo} alt="Google logo" width="20" height="20" />
-          Iniciar sesión con Google
-        </button>
+        <LoginButton onLogin={signInWithGoogle} />
       </div>
       {searchParams?.message && (
         <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
--- app/(home)/my-review/[id]/complete/page.tsx
+++ app/(home)/my-review/[id]/complete/page.tsx
@@ -3,19 +3,36 @@ import Image from 'next/image';
 
 const MyReviewCompletePage: React.FC = async () => {
   return (
-    <div className="flex flex-col justify-center items-center flex-grow">
-      <div>
+    // <div className="flex flex-col justify-center items-center flex-grow">
+    //   <div>
+    //     <Image
+    //       src="/images/axel-fiesta.png"
+    //       width={250}
+    //       height={250}
+    //       alt="Picture of an employeer celebrating with balloons and confetti."
+    //     />
+    //   </div>
+    //   <div>
+    //     ¡Gracias por haber completado la evaluación! En breve recibirás un
+    //     correo de confirmación. Una vez que ambos hayan completado la
+    //     evaluación, podrás ver los resultados en esta página.
+    //   </div>
+    // </div>
+    <div className="flex items-center justify-center h-full">
+      <div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg max-w-sm w-full overflow-hidden">
         <Image
           src="/images/axel-fiesta.png"
           width={250}
           height={250}
           alt="Picture of an employeer celebrating with balloons and confetti."
         />
-      </div>
-      <div>
-        ¡Gracias por haber completado la evaluación! En breve recibirás un
-        correo de confirmación. Una vez que ambos hayan completado la
-        evaluación, podrás ver los resultados en esta página.
+        <div className="p-4">
+          <p className="text-center">
+            ¡Gracias por haber completado la evaluación! En breve recibirás un
+            correo de confirmación. Una vez que ambos hayan completado la
+            evaluación, podrás ver los resultados en esta página.
+          </p>
+        </div>
       </div>
     </div>
   );
