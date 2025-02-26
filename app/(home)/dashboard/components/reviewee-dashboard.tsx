34a884d82a8b0da54f87e3f85d0627a3d551d69d
Ivan Nizetic <ivan.nizetic@gmail.com>
2025-02-26 17:48:02 -0300

    Feat: added shadcn components, updated primary color, removed dashboard routes

--- app/(home)/dashboard/components/reviewee-dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const RevieweeDashboard: React.FC = () => {
-  return (
-    <h2 className="text-2xl underline font-bold flex-grow">Mi Evaluacion</h2>
-  );
-};
-
-export default RevieweeDashboard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,75 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      animation: {
-        bounce200: 'bounce 1s infinite 200ms',
-        bounce400: 'bounce 1s infinite 400ms'
-      },
-      height: {
-        'screen-minus-header': 'calc(100vh - 3rem)'
-      },
-      backgroundImage: {
-        'devlights-bg': "url('/images/dev-login.png')"
-      },
-      colors: {
-        primary: '#8D408D',
-        secondary: '#5090CC',
-        secondaryAlt: '#3C7CBB',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))'
-        }
-      }
-    }
+  	extend: {
+  		animation: {
+  			bounce200: 'bounce 1s infinite 200ms',
+  			bounce400: 'bounce 1s infinite 400ms'
+  		},
+  		height: {
+  			'screen-minus-header': 'calc(100vh - 3rem)'
+  		},
+  		backgroundImage: {
+  			'devlights-bg': 'url('/images/dev-login.png')'
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			secondaryAlt: '#3C7CBB',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			btn: {
+  				background: 'hsl(var(--btn-background))',
+  				'background-hover': 'hsl(var(--btn-background-hover))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: []
+  plugins: [require("tailwindcss-animate")]
 };
